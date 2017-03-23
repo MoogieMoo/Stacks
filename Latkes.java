86b9eb20ddc308b594e03353619f3d2e5a7fc40b
MoogieMoo <yxia1@stuy.edu>
2017-03-23 00:45:38 -0400

    Add files via upload

--- Latkes.java
+++ Latkes.java
@@ -89,7 +89,7 @@ public class Latkes
     public String toString(){
 	String print = "[";
 	for( int i = 0; i < _stack.length; i++ ) {
-	    print += _stack[x];
+	    print += _stack[i];
 	    print += " ";
 	}
 	print  += "]";
