578edbafac28532cebb51a005f283cb16ca49981
Yi Xia <yxia1@stuy.edu>
2017-03-23 08:39:37 -0400

    aaah

--- Latkes.java
+++ Latkes.java
@@ -5,11 +5,10 @@
 
 /***
  * class Latkes
- * SKELETON
  * (actually: a class to implement a stack of Strings)
  * uses an array as underlying data structure
  ***/
-
+ 
 public class Latkes 
 {
 
@@ -17,104 +16,92 @@ public class Latkes
     private int _stackSize;
 
     //constructor
-    public Latkes( int size ) 
-    {
-        _stack = new String[ size ];
+    public Latkes( int capacity ) 
+    { 
+	_stack = new String[ capacity ];
 	_stackSize = 0;
     }
 
+    
     //means of insertion
     public void push( String s ) 
     {
-	//no items
-        if( _stack[0] == null ) {
-	    _stack[0] = s;
-	    _stackSize += 1;
-	}
-	//shift down
-	else if( _stack[ _stack.length - 1 ] == null ) {
-	    int shove = _stack.length-1;
-	    while( shove > 0 ) {
-		_stack[ shove ] = _stack[ shove - 1 ];
-		shove -= 1;
+	//create more storage space if necessary
+	if ( isFull() ) {
+	    String [] temp = new String[ _stack.length * 2 ];	
+	    for( int i = 0; i < _stack.length; i++ ) {
+		temp[i] = _stack[i];
 	    }
-	    _stack[0] = s;
-	    _stackSize += 1;
+	    _stack = temp;
 	}
-	else {
-	    System.out.println( "Error" );
-	    return;
-	}
+	_stack[ _stackSize ] = s;
+	_stackSize++;
     }
 
+    
     //means of removal
     public String pop( ) 
-    {
-	//first item gets removed
-	String pup = _stack[0];
-	//shift up
-	int pip = 0;
-	while ( pip < _stack.length - 1 ){
-	    _stack[pip] = _stack[ pip + 1 ];
-	    pip += 1;
-	}
-	_stack[ _stack.length - 1 ] = null;
-	_stackSize -= 1;
-	//returns removed item
-	return pup;
+    { 
+	String retStr = "";
+	retStr = _stack[_stackSize-- -1];
+	return retStr;
     }
 
+    //reverse a String
+    public static String flip( String s ) {
+	String [] flipped;
+	flipped = new String[ capacity ];
+	while( !s.isEmpty() ) {
+	    flipped.push( pop() );
+	}	
+	return flipped;
+    }
+
+    //check for matched parens
+    public static boolean allMatched( String s ) {
+	return false;
+    }
+    
     //chk for emptiness
     public boolean isEmpty() 
-    {
-	if( _stackSize == 0 ) {
-	    return true;
-	}
-	else {
-	    return false;
-	}
+    { 
+	return _stackSize == 0;
     }
 
     //chk for fullness
     public boolean isFull() 
     {
-	if( _stackSize == _stack.length ) {
-	    return true;
-	}
-	else {
-	    return false;
-	}
+	return _stackSize >= _stack.length; 
     }
 
-    public String toString(){
-	String print = "~~~~~~~~~~~~";
-	for( int i = 0; i < _stack.length; i++ ) {
-	    print += "\n";
-	    print += _stack[i];
-	}
-	print += "~~~~~~~~~~~~";
-	return print;
-    }
+    public static void main( String[] args ) {
 
-    //test
-    public static void main(String[] args){
+	Latkes tastyStack = new Latkes(10);
 
-	//instantiate stack with a 20 pancake capacity
-	Latkes flapjacks = new Latkes(20);
-	System.out.println(flapjacks.isEmpty());//should return true
-	System.out.println(flapjacks.isFull());//should return false b/c it is empty
-	//should print empty stack
-	System.out.println(flapjacks.toString());
+	tastyStack.push("foo");
+	tastyStack.push("goo");
+	tastyStack.push("hoo");
+	tastyStack.push("joo");
+	tastyStack.push("moo");
+	tastyStack.push("loo");
+	tastyStack.push("poo");
+	tastyStack.push("qoo");
+	tastyStack.push("zoo");
+	tastyStack.push("choo");
+	tastyStack.push("coocoo");
 
-	//keep adding pancakes until full
-	while ( flapjacks._stackSize < 20 ){
-	    flapjacks.push( "pancake" );
-	}
-
-	//should print stack of 20 pancakes
-	System.out.println(flapjacks.toString());
-    }
-
-
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+	System.out.println( tastyStack.pop() );
+    }//end main
+    
 }//end class Latkes
 
